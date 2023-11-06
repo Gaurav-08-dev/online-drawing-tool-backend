bb109371956f45d0b79210649e801af8435ae740
gaurav-08-dev <g8111997@gmail.com>
2023-11-06 21:34:12 +0530

    Add socket connections for drawing , changing config, erasing

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
-const cors = require('cors')
+const cors = require('cors');
 
 
-const app = express()
-app.use(cors({origin:'http://localhost:8080' }))
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:8080' }));
 const httpServer = createServer(app);
-const io = new Server(httpServer, {cors: 'http://localhost:8080'})
+const io = new Server(httpServer, { cors: 'http://localhost:8080' });
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
 
-    console.log("Server Connected")
+socket.on("beginPath", (arg)=>{
+    socket.broadcast.emit('beginPath', arg)
 })
+
+socket.on("drawLine", (arg)=>{
+    socket.broadcast.emit('drawLine',arg)
+})
+
+socket.on("changeConfig", (arg)=>{
+    socket.broadcast.emit('changeConfig',arg)
+})
+
+})
+
 
 httpServer.listen(5000)
